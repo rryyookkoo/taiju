5981b1d06ffd0353ae18dce048ce2605dc6664b5
Ryoko <ryoko0425@gmail.com>
2015-04-03 21:52:16 +0900

    Hello World commit

--- src/main/java/taiju/Main.java
+++ src/main/java/taiju/Main.java
@@ -1,9 +1,23 @@
 package taiju;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("これはテスト");
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
 }
