c6ddc5591a45d749b5c9ff01716e7043729f15bf
Ryoko <ryoko0425@gmail.com>
2015-04-04 14:04:59 +0900

    add screen tranditon

---
+++ src/main/java/taiju/form/TaijuForm.java
@@ -0,0 +1,31 @@
+package taiju.form;
+
+public class TaijuForm {
+    private String id;
+    private String recordedAt;
+    private String weight;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRecordedAt() {
+        return recordedAt;
+    }
+
+    public void setRecordedAt(String recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+}
--- src/main/java/taiju/web/TaijuController.java
+++ src/main/java/taiju/web/TaijuController.java
@@ -3,8 +3,11 @@ package taiju.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import taiju.domain.Taiju;
+import taiju.form.TaijuForm;
 import taiju.service.TaijuService;
 
 import java.util.List;
@@ -19,6 +22,16 @@ public class TaijuController {
         List<Taiju> list = taijuService.findAll();
         model.addAttribute("taijuList", list);
         return "taiju/index";
+    }
+
+    @RequestMapping("/taiju/new")
+    String newTaiju(TaijuForm taijuForm) {
+        return "taiju/new";
+    }
+
+    @RequestMapping(value="/taiju/create",method = RequestMethod.POST)
+    String createTaiju(TaijuForm taijuForm){
+        return "redirect:/";
     }
 
 }
