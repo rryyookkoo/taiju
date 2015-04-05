79e7ef29016ba951d57091db9fe4f7b8032d2851
Ryoko <ryoko0425@gmail.com>
2015-04-05 20:38:03 +0900

    added userRepository

---
+++ src/main/java/taiju/domain/User.java
@@ -0,0 +1,45 @@
+package taiju.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private long createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/taiju/repository/UserRepository.java
+++ src/main/java/taiju/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package taiju.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import taiju.domain.User;
 
-public interface UserRepository {//extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
