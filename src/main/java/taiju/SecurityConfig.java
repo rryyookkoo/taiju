add2d05545e03b9fea483f249487066c06af137f
Ryoko <ryoko0425@gmail.com>
2015-04-05 12:52:18 +0900

    security ok

--- src/main/java/taiju/SecurityConfig.java
+++ src/main/java/taiju/SecurityConfig.java
@@ -1,28 +1,37 @@
 package taiju;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+
+@Configuration
+@EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                        //.antMatchers("/css/**").permitAll() // 認証不要なパス。なければ書かなくてOK
-                .anyRequest().authenticated().and().formLogin()
-                .loginPage("/login") // ログインページのURLは /login で
-                .permitAll() // 認証不要
-                .and()
+                    //.antMatchers("/css/**").permitAll() // 認証不要なパス。なければ書かなくてOK
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login") // ログインページのURLは /login で
+                    .permitAll() // 認証不要
+                    .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // ログアウトページのパスは/logoutで
-                .permitAll(); // 認証不要
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // ログアウトページのパスは/logoutで
+                    .permitAll(); // 認証不要
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth
+                .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
     }
 }
